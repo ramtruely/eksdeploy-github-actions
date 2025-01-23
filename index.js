5cd3b95040cfb91b871a415ac541f18dceab6fc3
ramtruely <ramtruely6@gmail.com>
2025-01-23 22:32:03 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, World! This is Node.js on AWS EKS!');
+  res.send('Hello, World! This is Node.js on AWS EKS again!');
 });
 
 const PORT = process.env.PORT || 3000;
